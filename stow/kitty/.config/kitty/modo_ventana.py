80c54230c5f0996b28ae9ef973cfbdafe6ece2d8
Facu <gabylope05@gmail.com>
2026-07-01 08:49:24 -0300

    fix: abrir splits en el mismo directorio de trabajo de la ventana actual

--- stow/kitty/.config/kitty/modo_ventana.py
+++ stow/kitty/.config/kitty/modo_ventana.py
@@ -9,9 +9,9 @@ def main(args):
 def handle_result(args, result, target_window_id, boss):
     action = args[1] if len(args) > 1 else "vsplit"
     if action == "vsplit":
-        boss.call_remote_control(None, ("launch", "--location=vsplit"))
+        boss.call_remote_control(None, ("launch", "--location=vsplit", "--cwd=current"))
     elif action == "hsplit":
-        boss.call_remote_control(None, ("launch", "--location=hsplit"))
+        boss.call_remote_control(None, ("launch", "--location=hsplit", "--cwd=current"))
     elif action == "close":
         boss.call_remote_control(None, ("close-window",))
     boss.pop_keyboard_mode()
