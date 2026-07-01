0ff553b5a3d0bdaddd1b0fc264124bb26f379949
Facu <gabylope05@gmail.com>
2026-07-01 07:49:14 -0300

    feat: gestionar splits y cierre de ventanas con script que auto-exit del modo wm

---
+++ stow/kitty/.config/kitty/modo_ventana.py
@@ -0,0 +1,17 @@
+from kittens.tui.handler import result_handler  # type: ignore
+
+
+def main(args):
+    pass
+
+
+@result_handler(no_ui=True)
+def handle_result(args, result, target_window_id, boss):
+    action = args[1] if len(args) > 1 else "vsplit"
+    if action == "vsplit":
+        boss.call_remote_control(None, ("launch", "--location=vsplit"))
+    elif action == "hsplit":
+        boss.call_remote_control(None, ("launch", "--location=hsplit"))
+    elif action == "close":
+        boss.call_remote_control(None, ("close-window",))
+    boss.pop_keyboard_mode()
